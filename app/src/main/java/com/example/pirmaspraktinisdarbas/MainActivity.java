48599b789b7c8a67347118c8ad64e59e80118adc
lauratmanaviciute <lauraa.atm@gmail.com>
2023-03-04 18:07:31 +0200

    Added changeText button and implemented logic

--- app/src/main/java/com/example/pirmaspraktinisdarbas/MainActivity.java
+++ app/src/main/java/com/example/pirmaspraktinisdarbas/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.pirmaspraktinisdarbas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvMain;
+    Button btnChangeMainText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvMain = findViewById(R.id.tvMain);
+
+        this.btnChangeMainText = (Button) findViewById(R.id.btnChangeMainText);
+        btnChangeMainText.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                tvMain.setText("Viso gero!");
+            }
+        });
     }
 }
